593d2dfcaa0ae79e7ea149e46d3054f6eb5758b4
P.Meghana <152874646+PappuMeghana@users.noreply.github.com>
2024-01-03 22:32:22 +0530

    #include<stdio.h> int main() {     int h,m;     scanf("%d%d",&h,&m);     int minutesbeforenewyear=(24-h-1)*60+(60-m);     printf("%d",minutesbeforenewyear); }

--- Happy_New__Year.c
+++ Happy_New__Year.c
@@ -4,6 +4,5 @@ int main()
     int h,m;
     scanf("%d%d",&h,&m);
     int minutesbeforenewyear=(24-h-1)*60+(60-m);
-    printf("%d
-",minutesbeforenewyear);
+    printf("%d",minutesbeforenewyear);
 }
